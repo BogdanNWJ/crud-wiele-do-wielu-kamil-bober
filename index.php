489bc7091ab6ee25d8ad67145c139877a60a04d3
BogdanNWJ <42960709+BogdanNWJ@users.noreply.github.com>
2020-10-28 12:47:27 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -3,20 +3,47 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WIELE DO WIELU</title>
     <link rel="stylesheet" href="style.css">
+    <title></title>
 </head>
 <body>
 
+<div class="cont">
+    <div class="head"><h1>1 Kamil Bober gr1</h1><h3>Autor</h3>
+          <form action="insert1.php" method="post">
+              <input type="text" name="autor" >
+              <input type="submit" value="Zapisz">
+          </form>
+          <h3>Tytuł</h3>
+          <form action="insert2.php" method="post" >
+              <input type="text" name="tytul" >
+              <input type="submit" value="Zapisz">   
+              </form>
+              <h3>lista</h3>
+         <?php
+          $conn = new mysqli("sql7.freemysqlhosting.net", "sql7373173", "k9GUvMpjAd", "sql7373173");
+           $result2 = $conn->query("SELECT * FROM autorzy");
 
-    <header><h1>Kamil Bober gr1</h1></header>
-    <main>
-    <?php
-        $servername = "sql7.freemysqlhosting.net";
-        $username = "sql7373173";
-        $password = "k9GUvMpjAd";
-        $dbname = "sql7373173";
+           echo("<form action='insert3.php' method='POST'  >");
+           echo("<select name='auts'>");
+           while($row=$result2->fetch_assoc() ){
+           echo("<option value='".$row['id_autor']."'>".$row['autor']."</option>");
+           }
+           echo("</select>");
 
+           $result3 = $conn->query("SELECT * FROM tytuly");
+
+           echo("<select name='tyts'>");
+           while($row=$result3->fetch_assoc() ){
+           echo("<option value='".$row['id_tytul']."'>".$row['tytul']."</option>");
+           }           
+           echo("</select>");
+
+           echo("<input type='submit' value='Zapisz'>");
+           echo("</form>");
+         ?></div>
+    <div class="main">
+          <?php
            $conn = new mysqli("sql7.freemysqlhosting.net", "sql7373173", "k9GUvMpjAd", "sql7373173");
            $result1 = $conn->query("SELECT id_krzyz, autor, tytul FROM krzyz, autorzy, tytuly WHERE krzyz.id_autor=autorzy.id_autor AND krzyz.id_tytul=tytuly.id_tytul");
             
@@ -36,14 +63,14 @@
                echo("</tr>");
            }
           ?>
+        </div>
+    <div class="ins">
+          
+          
+   </div>
+   <div class="d"></div>
+        
     </div>
-</main>
-<nav>
-
-
-</nav>
-
-<footer></footer>
+    
 </body>
-<script src="main.js"></script>
 </html>
